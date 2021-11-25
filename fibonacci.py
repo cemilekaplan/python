9074fdf61dfcc60667bddeebff41c3d2385a89a1
cemilekaplan <cmlkcygt@gmail.com>
2021-11-25 12:16:53 +0300

    fibonacci series added

--- fibonacci.py
+++ fibonacci.py
@@ -1,8 +1,10 @@
+"""
 Task : Create a list consisting of Fibonacci numbers from 1 to 55 using control flow statements.
 
 The desired output is like :
 
 fibonacci →  [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+"""
 
 
 
