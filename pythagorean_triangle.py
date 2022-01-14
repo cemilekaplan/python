9fa1f0efa5b9f2862b72c88a43f656b4254d4c51
cemilekaplan <93863632+cemilekaplan@users.noreply.github.com>
2022-01-14 13:20:02 +0300

    pythagoren_triangle python code added

---
+++ pythagorean_triangle.py
@@ -0,0 +1,10 @@
+'''
+Write a program that prints the numbers from 1 to 100 that form a pythagorean triangle. 
+For example:3 4 5
+'''
+
+for x in range(1,100) :
+  for y in range(x,100) :
+    for z in range(y,100) :
+      if x ** 2 + y **2 == z ** 2 :
+        print(x,y,z)
