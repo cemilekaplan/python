71d22ded783dc361d0d1ebf077bbeed8fc483817
cemilekaplan <cmlkcygt@gmail.com>
2022-02-16 16:06:36 +0300

    updated

--- phonebook.py
+++ phonebook.py
@@ -1,3 +1,6 @@
+from tokenize import Name
+
+
 def print_menu():
     print('1. Find phone number')
     print('2. Insert a phone number')
@@ -13,23 +16,28 @@ while menu_choice != 4:
     menu_choice = int(input("Type in a number (1-4): "))
     
     if menu_choice == 2:
-        print("Insert Name and Number")
+        print("Insert Name of the person")
         name = input("Name: ")
+        print("Insert phone number of the person")
         phone = input("Number: ")
         numbers[name] = phone
+        print("Phone number of {} is inserted into the phonebook".format(name))
     elif menu_choice == 3:
-        print("Delete Name and Number")
-        name = input("Name: ")
+        print("Whom to delete from phonebook :")
+        name = input("")
         if name in numbers:
             del numbers[name]
+            print("{} is deleted from the phonebook".format(name))
         else:
-            print(name, "was not found")
+            print(name, "'s phone number couldn't find")
     elif menu_choice == 1:
-        print("Find Number")
-        name = input("Name: ")
+        print("Find the phone number of : ")
+        name = input("")
         if name in numbers:
             print("The number is", numbers[name])
         else:
-            print(name, "was not found")
+            print(name, "'s phone number couldn't find")
     elif menu_choice != 4:
         print_menu()
+if menu_choice == 4:
+    print("Exiting Phonebook")
