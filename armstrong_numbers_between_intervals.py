856cdf56aa629237b43ad9b004848c9a890c8405
cemilekaplan <cmlkcygt@gmail.com>
2022-02-27 11:57:18 +0300

    armstrong_numbers_between_intervals.py added

---
+++ armstrong_numbers_between_intervals.py
@@ -0,0 +1,17 @@
+"""
+write a program to find all the Armstrong numbers present in between two intervals in Python. 
+For example, Armstrong numbers between 100 and 2000. 
+Expected output:
+153
+370
+371
+407
+1634
+
+"""
+
+
+lower_num, higher_num = list(map(int, input("").split()))
+for i in range(lower_num, higher_num+1) :
+    if sum([int(j) ** len(str(i)) for j in str(i)]) == i :
+        print(i)
